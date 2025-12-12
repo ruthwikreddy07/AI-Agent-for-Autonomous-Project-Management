186cbc7ed22d4747709594cda45a1ab0b6734303
ruthwikreddy07 <179065000+ruthwikreddy07@users.noreply.github.com>
2025-12-12 13:50:21 +0530

    calender issue

--- frontend-dashboard/src/environments/environment.ts
+++ frontend-dashboard/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ai-manager-backend.onrender.com'  // <-- YOUR RENDER URL
+  apiUrl: 'https://ai-manager-backend.onrender.com'  // <-- YOUR RENDER URL OR  FOR LOCAL TESTING http://127.0.0.1:8000
 };
